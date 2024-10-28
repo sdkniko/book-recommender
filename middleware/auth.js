55ec5cd6797b9985c24ccf246b18885d9554d255
vito gastaldi <46353610+sdkniko@users.noreply.github.com>
2024-10-28 17:32:51 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,22 @@
+// middleware/auth.js
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const auth = (req, res, next) => {
-    try {
-        // Obtener el token del header
-        const authHeader = req.header('Authorization');
-        if (!authHeader) {
-            return res.status(401).send('Access denied. No token provided.');
-        }
+const auth = async (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
 
-        // Verificar si el token comienza con 'Bearer '
-        if (!authHeader.startsWith('Bearer ')) {
-            return res.status(401).send('Invalid token format.');
-        }
+  if (!token) {
+    return res.status(401).send('Acceso denegado. No se proporcionó token.');
+  }
 
-        // Extraer el token
-        const token = authHeader.substring(7); // Removes 'Bearer '
-
-        // Verificar el token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        // Asignar el usuario decodificado a req.user
-        req.user = decoded;
-        next();
-    } catch (error) {
-        res.status(401).send('Invalid token.');
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded._id);
+    if (!req.user) throw new Error();
+    next();
+  } catch (error) {
+    res.status(401).send('Token no válido');
+  }
 };
 
 module.exports = auth;
